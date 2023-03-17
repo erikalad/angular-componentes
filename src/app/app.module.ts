4586026bec85fc00a2f1ce1342f116df175d94e1
Erika <erikaladner5@gmail.com>
2023-03-17 20:54:41 -0300

    nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { ImgComponent } from './components/img/img.component';
 import { ProductComponent } from './components/product/product.component';
 import { ProductsComponent } from './components/products/products.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { ProductsComponent } from './components/products/products.component';
     ImgComponent,
     ProductComponent,
     ProductsComponent,
+    NavComponent,
 
   ],
   imports: [
