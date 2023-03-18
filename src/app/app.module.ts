d369ee1438a9d45670cc2957fbe57f58265ea70f
Erika <erikaladner5@gmail.com>
2023-03-18 10:50:05 -0300

    directivas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ProductsComponent } from './components/products/products.component';
 import { NavComponent } from './components/nav/nav.component';
 import { ReversePipe } from './pipes/reverse.pipe';
 import { TimeAgoPipe } from './pipes/time-ago.pipe';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { TimeAgoPipe } from './pipes/time-ago.pipe';
     NavComponent,
     ReversePipe,
     TimeAgoPipe,
+    HighlightDirective,
 
   ],
   imports: [
