c8ca5fde483b3e0e7acf6205d13af8fc81344c4b
Erika <erikaladner5@gmail.com>
2023-03-18 11:49:27 -0300

    carrito

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component , OnInit} from '@angular/core';
+import { StoreService} from './../../services/store.service'
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
 
+  constructor(
+    private storeService : StoreService
+  ){
+  }
   activeMenu = false;
-
+  counter =0;
   toggleMenu(){
     this.activeMenu = !this.activeMenu
+  }
+  ngOnInit(): void {
+    this.storeService.myCart$.subscribe(products => {
+    this.counter = products.length
+    })
   }
 
 }
--- src/app/services/store.service.ts
+++ src/app/services/store.service.ts
@@ -1,19 +1,21 @@
 import { Injectable } from '@angular/core';
 import {Product} from './../models/product.module'
-
+import { BehaviorSubject} from 'rxjs'
 @Injectable({
   providedIn: 'root'
 })
 export class StoreService {
 
   private myShoppingCart : Product[] = [];
+  private myCart = new BehaviorSubject<Product[]>([])
 
-
+  myCart$ = this.myCart.asObservable()
 
   constructor() { }
 
   addProduct(product:Product){
-    this.myShoppingCart.push(product)
+    this.myShoppingCart.push(product);
+    this.myCart.next(this.myShoppingCart)
 
   }
 
